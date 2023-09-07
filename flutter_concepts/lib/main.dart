e74e59207d86ddbb4115b8c0a77c00a26fe8bbec
Pheak02 <saingsopheak02@gmail.com>
2023-09-07 15:25:14 +0700

    remove not need import

--- flutter_concepts/lib/main.dart
+++ flutter_concepts/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_concepts/quiz.dart';
-import 'package:flutter_concepts/start_screen.dart';
 
 void main() {
   runApp(
--- flutter_concepts/lib/question_screen.dart
+++ flutter_concepts/lib/question_screen.dart
@@ -1,17 +1,38 @@
 // do return some text widget
 import 'package:flutter/material.dart';
 
-class QuestionScreen extends StatefulWidget{
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
   @override
-  State<QuestionScreen> createState(){
+  State<QuestionScreen> createState() {
     return _QuestionScreenState();
   }
 }
-class _QuestionScreenState extends State<QuestionScreen>{
+
+class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionScreen');
-
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('the question....',),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer1'),
+          ),
+           ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer2'),
+          ),
+           ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer3'),
+          ),
+        ],
+      ),
+    );
   }
 }
